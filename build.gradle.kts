5a2e2d6092b3f19aeb6583412c07a75c3f18f5b4
ad-open-reply <a.dellorco@reply.it>
2025-11-12 14:21:08 +0100

    [AD] Gradle build workflow with jacoco tests
    * [AD] removing only read permission
    
    * [AD] Grant execute permission for gradlew
    
    * [AD] changed name of propeties file to ignore it in the pipeline
    
    * [AD] changed java version to 21
    
    * [AD] changed java version to 8
    
    * [AD] jacoco integration
    
    * [AD] jacoco integration
    
    * [AD] jacoco root report

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,3 +16,26 @@ subprojects {
         }
     }
 }
+plugins {
+    id("java")
+    id("jacoco")
+    id("buildlogic.java-conventions")
+}
+jacoco {
+    toolVersion = "0.8.10"
+}
+tasks.register<JacocoReport>("jacocoRootReport") {
+    dependsOn(subprojects.map { it.tasks.named("test") })
+
+    val coverageSourceDirs = files(subprojects.map { it.the<SourceSetContainer>()["main"].allSource.srcDirs })
+    val executionDataFiles = files(subprojects.map { it.buildDir.resolve("jacoco/test.exec") })
+
+    sourceDirectories.setFrom(coverageSourceDirs)
+    classDirectories.setFrom(files(subprojects.map { it.the<SourceSetContainer>()["main"].output }))
+    executionData.setFrom(executionDataFiles)
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
--- monitoring/build.gradle.kts
+++ monitoring/build.gradle.kts
@@ -3,6 +3,8 @@
  */
 
 plugins {
+    id("java")
+    id("jacoco")
     id("buildlogic.java-conventions")
 }
 
@@ -13,3 +15,14 @@ dependencies {
 }
 
 description = "monitoring"
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
